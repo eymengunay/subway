a07966dcb0ccc6efc4a17f5f9c9dea3847d883e9
Eymen Gunay <eymen@egunay.com>
2014-05-19 15:56:34 +0200

    License updated

--- src/Subway/Command/ClearCommand.php
+++ src/Subway/Command/ClearCommand.php
@@ -3,8 +3,8 @@
 /*
  * This file is part of the Subway package.
  *
- * (c) 2014 Eymen Gunay <eymen@egunay.com>
- * 
+ * (c) Eymen Gunay <eymen@egunay.com>
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
