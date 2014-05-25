22445c2a00dada0e39b8415189227f723568ea24
Eymen Gunay <eymen@egunay.com>
2014-05-25 17:06:33 +0200

    Worker test added

---
+++ tests/Subway/Tests/WorkerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Subway package.
+ *
+ * (c) Eymen Gunay <eymen@egunay.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Subway\Tests;
+
+use Subway\Message;
+use Subway\Worker;
+use Subway\Test\TestCase;
+
+/**
+ * Worker class test
+ */
+class WorkerTest extends TestCase
+{
+    /**
+     * Test worker perform
+     */
+    public function testWorker()
+    {
+        $message = new Message('defult', 'Subway\Tests\Job\Md5Job', array('hello' => 'world'));
+
+        $worker = new Worker('id', $this->factory);
+        $result = $worker->perform($message);
+
+        $this->assertTrue($result);
+    }
+}
