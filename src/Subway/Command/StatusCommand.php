e5a5a8c5ad5db146d07f2c796aca7f32dd95115f
Eymen Gunay <eymen@egunay.com>
2014-05-25 14:49:56 +0200

    Live status option added

--- src/Subway/Command/StatusCommand.php
+++ src/Subway/Command/StatusCommand.php
@@ -12,6 +12,8 @@
 namespace Subway\Command;
 
 use Subway\Factory;
+use React\EventLoop\Factory as React;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -28,6 +30,7 @@ class StatusCommand extends RedisAwareCommand
         $this
             ->setName('status')
             ->setDescription('Show subway status')
+            ->addOption('live', 'l', InputOption::VALUE_NONE, 'Live status')
         ;
 
         parent::configure();
@@ -37,27 +40,88 @@ class StatusCommand extends RedisAwareCommand
      * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
+    {   
+        if ( (bool)$input->getOption('live') ) {
+            declare(ticks=1);
+            $loop = React::create();
+
+            $loop->addPeriodicTimer(1, function () use ($input, $output) {
+                $status = $this->status($input, $output);
+                $output->writeln(json_encode($status, JSON_PRETTY_PRINT));
+            });
+
+            $loop->run();
+        } else {
+            $output->writeln(json_encode($this->status($input, $output), JSON_PRETTY_PRINT));
+        }
+    }
+
+    /**
+     * Status
+     * 
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return array
+     */
+    protected function status(InputInterface $input, OutputInterface $output)
+    {
+        return array(
+            'queue' => $this->queueStatus($input, $output),
+            'worker' => $this->workerStatus($input, $output)
+        );
+    }
+
+    /**
+     * Queue status
+     * 
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return array
+     */
+    protected function queueStatus(InputInterface $input, OutputInterface $output)
     {
         $rows = array();
 
         $factory = new Factory($this->redis);
         foreach ($factory->getQueues() as $queue) {
-            $rows[] = array(
-                $queue->getName(),
-                $queue->count()
-            );
+            $rows[$queue->getName()] = $queue->count();
         }
 
         $delayedQueue   = $factory->getDelayedQueue();
         $repeatingQueue = $factory->getRepeatingQueue();
-        $rows[] = array($delayedQueue->getName(), $delayedQueue->count());
-        $rows[] = array($repeatingQueue->getName(), $repeatingQueue->count());
 
-        $table = $this->getHelperSet()->get('table');
-        $table
-            ->setHeaders(array('Queue', 'Jobs'))
-            ->setRows($rows)
-        ;
-        $table->render($output);
+        if ($delayedCount = $delayedQueue->count()) {
+            $rows[$delayedQueue->getName()] = $delayedCount;
+        }
+
+        if ($repeatingCount = $repeatingQueue->count()) {
+            $rows[$repeatingQueue->getName()] = $repeatingCount;
+        }
+
+        return $rows;
+    }
+
+    /**
+     * Worker status
+     * 
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return array
+     */
+    protected function workerStatus(InputInterface $input, OutputInterface $output)
+    {
+        $rows = array();
+
+        $factory = new Factory($this->redis);
+        foreach ($factory->getWorkers() as $worker) {
+            list($host, $pid, $queues) = explode(':', $worker);
+            $rows[] = array(
+                'host'   => $host,
+                'pid'    => $pid,
+                'queues' => $queues,
+            );
+        }
+
+        return $rows;
     }
 }
